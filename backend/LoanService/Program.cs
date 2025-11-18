dda2e2ce22a3478ff4350a7eabda2a2ed264e9c4
51dollar <borushkoddima@mail.ru>
2025-11-18 21:31:44 +0300

    init backend project

---
+++ backend/LoanService/Program.cs
@@ -0,0 +1,22 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+app.UseCors();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
